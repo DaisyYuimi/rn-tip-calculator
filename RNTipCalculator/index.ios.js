d68c059a250f6f054eafb9774ef7bf8a8a093c7a
sophie <sophievictor55@gmail.com>
2017-04-29 14:35:08 +0700

    UI

--- RNTipCalculator/index.ios.js
+++ RNTipCalculator/index.ios.js
@@ -9,46 +9,70 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Keyboard,
+  TouchableWithoutFeedback,
+  TextInput
 } from 'react-native';
 
+import SegmentedControlTab from 'react-native-segmented-control-tab'
 
 export default class RNTipCalculator extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      billAmount: 0,
+      selectedTipIndex: 0
+    }
+  }
+  
+  componentDidMount() { 
+  }
+
+  handleAmountChange(value) {
+    this.setState({
+      billAmount: value
+    })      
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+      
+      <View>
+        <TouchableWithoutFeedback onPress={ () => {Keyboard.dismiss()} }>
+          <View>
+
+            <View>
+              <TextInput autoFocus={true} keyboardType='numeric' style={{height: 40}} onChangeText={ this.handleAmountChange.bind(this) } placeholder="Enter Bill amount"/>
+            </View>
+
+            <View>
+              <View>
+                <SegmentedControlTab
+                  values= {["10%", "20%", "30%"]}
+                  selectedIndex={1}
+                  onTabPress={ function(){ console.log("Tab pressed.")} }
+                />
+              </View>
+
+              <View>
+                <Text>Bill amount: {this.state.billAmount}</Text>
+                <Text>Tip amount: 0</Text>
+                <Text>Percent: 0</Text>
+              </View>
+
+              <View>
+                <Text>Result: 0</Text>
+              </View>
+            </View>
+          </View>
+        </TouchableWithoutFeedback>
       </View>
-    );
+    )
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 30,
-    textAlign: 'center',
-    margin: 0
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('RNTipCalculator', () => RNTipCalculator);
