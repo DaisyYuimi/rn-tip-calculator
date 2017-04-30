3df88d3d2d5795270b1caf84efd968359cc81cb1
sophie <sophievictor55@gmail.com>
2017-04-30 10:09:52 +0700

    add navigation

---
+++ RNTipCalculator/app/calculator-page.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Keyboard,
+  TouchableWithoutFeedback,
+  TextInput,
+  Slider,
+  Button
+} from 'react-native';
+
+export default class CalculatorPage extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      billAmount: 0,
+      selectedTipIndex: 0,
+      tipPercent: 10,
+      tipAmount: 0,
+      result: 0
+    }
+  }
+  
+  componentDidMount() { 
+  }
+
+  handleAmountChange(value) {
+    let billAmount = parseInt(value || 0);
+
+    this.setState({ billAmount: billAmount }, () => {
+      this.handleTipChanged(this.state.tipPercent);
+    });
+  }
+
+  handleTipChanged(value) {
+    let tipAmount = parseInt(value) / 100 * this.state.billAmount;
+    let result = tipAmount + this.state.billAmount;
+
+    this.setState({
+      tipPercent: value,
+      tipAmount: tipAmount,
+      result: result
+    });
+  }
+
+  render() {
+    return (
+      
+      <View>
+        <Button
+            style={{width:30, flex:0.1}}
+            title="Settings"
+            onPress={() => this.props.navigator.push({id:"BlankPage"})}
+          />
+
+        <TouchableWithoutFeedback onPress={ () => {Keyboard.dismiss()} }>
+          <View>
+
+            <View>
+              <TextInput autoFocus={true} keyboardType='numeric' style={{height: 40}} onChangeText={ this.handleAmountChange.bind(this) } placeholder="Enter Bill amount"/>
+            </View>
+
+            <View>
+              <View>
+                <Slider minimumValue={10} maximumValue={50} step={10} onValueChange={ this.handleTipChanged.bind(this) } />
+              </View>
+
+              <View>
+                <Text>Bill amount: {this.state.billAmount}</Text>
+                <Text>Tip percent: {this.state.tipPercent}%</Text>
+                <Text>Tip amount: {this.state.tipAmount.toFixed(1)}</Text>
+                
+              </View>
+
+              <View>
+                <Text>Result: {this.state.result}</Text>
+              </View>
+            </View>
+          </View>
+        </TouchableWithoutFeedback>
+      </View>
+    )
+  }
+}
--- RNTipCalculator/index.ios.js
+++ RNTipCalculator/index.ios.js
@@ -13,77 +13,54 @@ import {
   Keyboard,
   TouchableWithoutFeedback,
   TextInput,
-  Slider
+  Slider,
+  Navigator,
+  Button
 } from 'react-native';
 
-import SegmentedControlTab from 'react-native-segmented-control-tab'
+import CalculatorPage from './app/calculator-page'
 
 export default class RNTipCalculator extends Component {
 
   constructor(props) {
     super(props);
 
-    this.state = {
-      billAmount: 0,
-      selectedTipIndex: 0,
-      tipPercent: 10,
-      tipAmount: 0,
-      result: 0
-    }
+    this.state = {}
   }
   
   componentDidMount() { 
   }
 
-  handleAmountChange(value) {
-    let billAmount = parseInt(value || 0);
-
-    this.setState({ billAmount: billAmount }, () => {
-      this.handleTipChanged(this.state.tipPercent);
-    });   
-  }
-
-  handleTipChanged(value) {
-    let tipAmount = parseInt(value) / 100 * this.state.billAmount;
-    let result = tipAmount + this.state.billAmount;
-
-    this.setState({
-      tipPercent: value,
-      tipAmount: tipAmount,
-      result: result
-    });
-  }
-
   render() {
     return (
+      <Navigator
+      initialRoute={{id: 'CalculatorPage', title: 'Tip Calculator Page'}}
+      renderScene={(route, navigator) => {
+        switch (route.id) {
+          case 'CalculatorPage':
+            return <CalculatorPage navigator={navigator} />
+            break;
+          case 'BlankPage':
+            return (
+              <View>
+                <Button
+                  style={{width:10, flex:0.1}}
+                  title="Go Back"
+                  onPress={() => navigator.pop({id:"CalculatorPage"})}
+                />
+
+                <View style={{flexDirection:'column'}}>
+                  <Text>Fck ya !!! I'm a blank page, I have nothing !</Text>
+                </View>
+
+              </View>
+            )
+            break;
+          default:
+        }
+      }}
+    />
       
-      <View>
-        <TouchableWithoutFeedback onPress={ () => {Keyboard.dismiss()} }>
-          <View>
-
-            <View>
-              <TextInput autoFocus={true} keyboardType='numeric' style={{height: 40}} onChangeText={ this.handleAmountChange.bind(this) } placeholder="Enter Bill amount"/>
-            </View>
-
-            <View>
-              <View>
-                <Slider minimumValue={10} maximumValue={50} step={10} onValueChange={ this.handleTipChanged.bind(this) } />
-              </View>
-
-              <View>
-                <Text>Bill amount: {this.state.billAmount}</Text>
-                <Text>Tip percent: {this.state.tipPercent}%</Text>
-                <Text>Tip amount: {this.state.tipAmount.toFixed(1)}</Text>
-                
-              </View>
-
-              <View>
-                <Text>Result: {this.state.result}</Text>
-              </View>
-            </View>
-          </View>
-        </TouchableWithoutFeedback>
-      </View>
     )
   }
 }
